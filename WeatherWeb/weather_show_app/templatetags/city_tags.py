bfe79bce5da8f4c6119264b53f002b4bf5220238
zhengyiming <realzhengyiming@outlook.com>
2021-04-19 23:01:33 +0800

    增加出行指南建议和穿衣建议

--- WeatherWeb/weather_show_app/templatetags/city_tags.py
+++ WeatherWeb/weather_show_app/templatetags/city_tags.py
@@ -61,13 +61,18 @@ def get_state(today_weather):
 
 # 出行指南
 @register.filter(name='state_to_outdoor_guide')
-def state_to_outdoor_guide(state: str):
+def state_to_outdoor_guide(state):
+    if not state:
+        return "无推荐"
     return walk_out_guide_dict[state]
 
 
 # 穿衣指数
 @register.filter(name='wear_clothing_guide')
 def wear_clothing_guide(min_temperature):
+    # print(min_temperature)
+    # return min_temperature
+    min_temperature = int(min_temperature)
     if min_temperature <= 0:
         return "棉衣、冬大衣、皮夹克、厚呢外套、呢帽、手套、羽绒服、裘皮大衣"
     elif min_temperature > 0 and min_temperature <= 5:
@@ -81,4 +86,4 @@ def wear_clothing_guide(min_temperature):
     elif min_temperature > 25 and min_temperature < 30:
         return '轻棉织物制作的短衣、短裙、薄短裙、短裤'
     else:
-        return "短袖"
+        return "没有推荐"
